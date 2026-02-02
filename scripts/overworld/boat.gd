3908c7d7041326a58fea1d1b4c57bc6280a480b0
anna ryplewski <ryplewsa@oregonstate.edu>
2026-02-01 18:52:38 -0800

    fixed NPC system
    added some models, changed the frog NPC to a shipping dock, fixed activation zone and logic, resized UI (unstyled)

--- scripts/overworld/boat.gd
+++ scripts/overworld/boat.gd
@@ -1,53 +1,41 @@
 extends RigidBody3D
 
-@export var ui_node: NodePath
-@onready var ui = get_node(ui_node)
-
+var nearby_npc: Node = null
 var mouse_movement = Vector2()
-var current_npc: Node = null
-var in_range: bool = false
-
 @onready var fish_notification = $"../FishNotification"  # If FishNotification is a sibling node
 @onready var detection_area = $DetectionArea  # Area3D child node
+@onready var ui = $"../UI"
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	detection_area.body_entered.connect(_on_area_entered)
 	detection_area.body_exited.connect(_on_area_exited)
-	
-	# Hide notification at start
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	if fish_notification:
 		fish_notification.visible = false
 
-func _input(event: InputEvent):
-	if event.is_action_pressed("ui_cancel"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	if event.is_action_pressed("interact") and current_npc and in_range:
-		if current_npc.has_method("try_interact"):
-			current_npc.try_interact()
-	
+		
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		mouse_movement += event.relative
 		
 func _physics_process(_delta):
+	if get_tree().paused:
+		return
+		
 	if mouse_movement != Vector2():
 		$H.rotation_degrees.y += -mouse_movement.x
 		mouse_movement = Vector2()
 		
 	if Input.is_action_pressed("w"):
 		apply_central_force(global_transform.basis * Vector3.LEFT * 20)
-		if Input.is_action_pressed("a"):
-			apply_torque(Vector3(0, 1, 0))
-		if Input.is_action_pressed("d"):
-			apply_torque(Vector3(0, -1, 0))
 	elif Input.is_action_pressed("s"):
 		apply_central_force(global_transform.basis * Vector3.RIGHT * 10)
-		if Input.is_action_pressed("a"):
-			apply_torque(Vector3(0, 1, 0))
-		if Input.is_action_pressed("d"):
-			apply_torque(Vector3(0, -1, 0))
-			
+	if Input.is_action_pressed("a"):
+		apply_torque(Vector3(0, 1, 0))
+	elif Input.is_action_pressed("d"):
+		apply_torque(Vector3(0, -1, 0))
+
 	if $Floaty.global_transform.origin.y <= 0:
 		apply_force(Vector3.UP * 20 * -$Floaty.global_transform.origin.y, $Floaty.global_transform.origin - global_transform.origin)
 	if $Floaty2.global_transform.origin.y <= 0:
@@ -57,16 +45,29 @@ func _physics_process(_delta):
 	if $Floaty4.global_transform.origin.y <= 0:
 		apply_force(Vector3.UP * 20 * -$Floaty4.global_transform.origin.y, $Floaty4.global_transform.origin - global_transform.origin)
 
+	if Input.is_action_just_pressed("interact"):
+		if nearby_npc and not ui.is_dialogue_open():
+			ui.start_dialogue(nearby_npc.npc_lines)
+		elif ui.is_dialogue_open():
+			ui.advance_dialogue()
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
 # Fish and NPC notification area detection
 func _on_area_entered(body):
+	if body == self:
+		return
 	if body.name == "RipplingWaterArea": 
 		show_fish_notification()
-	if body.is_in_group("npc"):
-		set_current_npc(body)
-		
+	elif body.is_in_group("npc"):
+		nearby_npc = body
+		ui.show_prompt()
+
 func _on_area_exited(body):
-	if body.is_in_group("npc"):
-		clear_current_npc(body)
+	if body.is_in_group("npc") and nearby_npc == body:
+		nearby_npc = null
+		ui.hide_prompt()
 
 func show_fish_notification():
 	if fish_notification:
@@ -74,20 +75,3 @@ func show_fish_notification():
 		# Notification will stay visible for 3 seconds
 		await get_tree().create_timer(3.0).timeout
 		fish_notification.visible = false
-
-func set_current_npc(npc):
-	current_npc = npc
-	in_range = true
-	if ui:
-		ui.player_enters_interzone()
-	var callable_dialogue = Callable(ui, "start_dialogue")
-	if not npc.is_connected("interaction_started", callable_dialogue):
-		npc.connect("interaction_started", callable_dialogue)
-	
-func clear_current_npc(npc):
-	if current_npc == npc:
-		current_npc = null
-		in_range = false
-		if ui:
-			ui.player_exits_interzone()
-		
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,23 +1,25 @@
 extends CanvasLayer
 
-@export var player_node: NodePath
-@onready var player = get_node(player_node)
-
 @onready var interact_prompt = $prompt
 @onready var dialogue_box = $dialoguebox
 @onready var dialogue_text = $dialoguebox/textbox
 
-var player_in_range: bool = false
-var dialogue_open: bool = false
+var player_in_range := false
+var dialogue_open := false
 var dialogue_lines: Array =  []
-var current_line: int = 0
+var current_line := 0
 
-# public methods	
-func player_enters_interzone():
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	dialogue_box.visible = false
+	interact_prompt.visible = false
+
+	
+func show_prompt():
 	player_in_range = true
 	_update_prompt()
 	
-func player_exits_interzone():
+func hide_prompt():
 	player_in_range = false
 	_update_prompt()
 	
@@ -28,31 +30,29 @@ func start_dialogue(lines: Array):
 	current_line = 0
 	dialogue_open = true
 	dialogue_box.visible = true
-	_display_current_line()
+	call_deferred("_show_current_line")
 	_update_prompt()
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
-func hide_dialogue():
+
+func advance_dialogue():
+	if not dialogue_open: 
+		return
+	current_line += 1
+	if current_line < dialogue_lines.size():
+		_show_current_line()
+	else:
+		close_dialogue()
+		
+func close_dialogue():
 	dialogue_open = false
 	dialogue_box.visible = false
 	_update_prompt()
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
-#internal
+func is_dialogue_open() -> bool:
+	return dialogue_open
+	
+func _show_current_line():
+	if dialogue_lines.size() > 0:
+		dialogue_text.text = dialogue_lines[current_line]
+	
 func _update_prompt():
 	interact_prompt.visible = player_in_range and not dialogue_open
-	
-func _display_current_line():
-	if current_line < dialogue_lines.size():
-		dialogue_text.text = dialogue_lines[current_line]
-		
-#input handling
-func _unhandled_input(event):
-	if dialogue_open and event.is_action_pressed("interact"):
-		current_line += 1
-		if current_line < dialogue_lines.size():
-			_display_current_line()
-		else:
-			hide_dialogue() 
-	elif dialogue_open and event.is_action_pressed("ui_cancel"):
-		hide_dialogue()
