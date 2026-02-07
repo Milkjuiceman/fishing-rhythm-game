d17db230187730988fb7b6ff5c03e67133a9c465
Tyschauermann <schauert@oregonstate.edu>
2026-02-06 20:41:46 -0800

    Calibrators
    Finished the logic for the input calibrator, added instructions.

--- rhythm/Calibrator/inp_calibrator_camera_3d.gd
+++ rhythm/Calibrator/inp_calibrator_camera_3d.gd
@@ -3,4 +3,4 @@ extends Camera3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.z += delta * 2
+	position.z += delta * 5
