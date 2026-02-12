ec0bdadec31f4796a3734f69d36a2a212a4255c9
Tyschauermann <schauert@oregonstate.edu>
2026-02-12 11:44:47 -0800

    Reroute level end to pixilate
    Changed the level to pixilate.tscn instead of main.

--- scripts/musiclevel/progress_bar.gd
+++ scripts/musiclevel/progress_bar.gd
@@ -20,4 +20,4 @@ func _on_referee_process(frame_state: FrameState) -> void:
 	prev_miss = frame_state.scorecard.misses
 	
 	if value < 0:
-		get_tree().change_scene_to_file("res://main.tscn")
+		get_tree().change_scene_to_file("res://graphics/pixelate.tscn")
