2b60d92a79566137132bada388c666695b34a38e
Tyschauermann <schauert@oregonstate.edu>
2026-04-16 15:59:40 -0700

    Resizing rhythm level hud
    Added the pixel art font and resizes the hud elements of the rhythm levels.

--- scripts/musiclevel/combo_counter.gd
+++ scripts/musiclevel/combo_counter.gd
@@ -6,19 +6,19 @@ func _on_referee_process(frame_state: FrameState) -> void:
 		return
 	var combo = frame_state.scorecard.combo
 	var multiplier = 1
-	var new_scale = Vector2(2,2)
+	var new_scale = Vector2(1,1)
 	
 	if combo >= 40:
 		multiplier = 8
-		new_scale = Vector2(5, 5)
+		new_scale = Vector2(2.5, 2.5)
 	elif combo >= 20:
 		multiplier = 4
-		new_scale = Vector2(4, 4)
+		new_scale = Vector2(2, 2)
 	elif combo >= 10:
 		multiplier = 2
-		new_scale = Vector2(3, 3)
+		new_scale = Vector2(1.5, 1.5)
 	else:
 		multiplier = 1
-		new_scale = Vector2(2, 2)
+		new_scale = Vector2(1, 1)
 	scale = new_scale
 	text = str("X", multiplier)
