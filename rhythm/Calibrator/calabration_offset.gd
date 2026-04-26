8c48a515f324867a4dea3615601c71dbe0186514
Tyschauermann <schauert@oregonstate.edu>
2026-04-26 15:24:06 -0700

    Finished Audio Calibrator

--- rhythm/Calibrator/calabration_offset.gd
+++ rhythm/Calibrator/calabration_offset.gd
@@ -1,10 +1,11 @@
 extends Label
 
-
+var sending = false
 
 func _on_referee_process(frame_state: FrameState) -> void:
 	
-	if frame_state.scorecard.hits >= 10:
+	if frame_state.scorecard.hits >= 10 and !sending:
+		sending = true
 		var audio_offset = -round(frame_state.scorecard.get_average_temporal_offset() * 100.0) / 100.0
 		var pause_menu = get_node_or_null("/root/PauseMenu")
 		pause_menu._on_audio_offset_changed(audio_offset)
