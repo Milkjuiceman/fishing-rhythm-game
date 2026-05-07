eedd3e35c88b08fe4eabad5356603082fd28f71f
Tyschauermann <schauert@oregonstate.edu>
2026-05-07 15:04:53 -0700

    Created End Credits scene

---
+++ scenes/credits/credits.gd
@@ -0,0 +1,10 @@
+extends Control
+
+var scroll_speed: int = 100
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$Credits.position.y -= delta * scroll_speed
+	
+	if $Credits.position.y < -1425:
+		set_process(false)
