20491274579b83b31fe01e4a7ff4e7fd17cdc0d6
Tyschauermann <schauert@oregonstate.edu>
2026-05-07 16:42:28 -0700

    Finished Credits

--- scenes/credits/credits.gd
+++ scenes/credits/credits.gd
@@ -1,10 +1,10 @@
 extends Control
 
-var scroll_speed: int = 100
+var scroll_speed: int = 50
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$Credits.position.y -= delta * scroll_speed
 	
-	if $Credits.position.y < -1425:
+	if $Credits.position.y < -1600:
 		set_process(false)
